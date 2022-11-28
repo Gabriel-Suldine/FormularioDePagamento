f7fc6662ff21e2d9e61a013259b659885ead0fe3
Gabriel-Suldine <gabriel.suldinne@gmail.com>
2022-11-27 21:28:22 -0300

    🎉 Funcional ainda nao estilizado

--- formularioDePagamento/main.js
+++ formularioDePagamento/main.js
@@ -1,5 +1,11 @@
 import './style.css'
-document.querySelector('#app').innerHTML = `
+const formulario = document.querySelector('#formulario');
+const numeroCartao = formulario.numeroCartao;
+const nome = formulario.nome;
+const dataDeVencimento = formulario.dataDeVencimento;
+const codigoSeguranca = formulario.ccv;
+
+document.querySelector('#cartao').innerHTML = `
 <div class="credit-card">
 <div class="credit-card-front">
     <div class="card-type">
@@ -9,19 +15,20 @@ document.querySelector('#app').innerHTML = `
     <img class="flag" src="./visa-logo.png">
 
     <div class="card-number">
-        #### #### #### ####
+      #0#0#0#0#0#0#0#0#0#0#
     </div>
     <div class="card-holder">
-        Fulano de Tal
+        nome Cliente
+
     </div>
     <div class="expiration">
-        06/2021
+         Data vencimento
     </div>
 </div>
 <div class="credit-card-back">
     <div class="stripe"></div>
     <div class="ccv-block">
-        123
+        ###
     </div>
     <div class="flag">
         <img class="flag" src="./visa-logo.png">
@@ -29,5 +36,39 @@ document.querySelector('#app').innerHTML = `
 </div>
 </div>
 `
+formulario.addEventListener('submit', event => {
+  event.preventDefault()   
+  document.querySelector('#cartao').innerHTML = `
+<div class="credit-card">
+<div class="credit-card-front">
+    <div class="card-type">
+        Cartão de Crédito
+    </div>
+
+    <img class="flag" src="./visa-logo.png">
+
+    <div class="card-number">
+        ${numeroCartao.value}
+    </div>
+    <div class="card-holder">
+        ${nome.value}
+
+    </div>
+    <div class="expiration">
+         ${dataDeVencimento.value}
+    </div>
+</div>
+<div class="credit-card-back">
+    <div class="stripe"></div>
+    <div class="ccv-block">
+        ${codigoSeguranca.value}
+    </div>
+    <div class="flag">
+        <img class="flag" src="./visa-logo.png">
+    </div>
+</div>
+</div>
+`
+})
 
 
